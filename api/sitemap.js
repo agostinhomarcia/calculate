a99dd501a056e7cfa5e8b3e7763bda05440fe3e7
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2025-04-06 19:48:58 -0300

    feat: Adiciona API route para sitemap

---
+++ api/sitemap.js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+  // Define o cabeçalho como XML
+  res.setHeader('Content-Type', 'application/xml');
+
+  // Cria o XML do sitemap
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://calculate-dusky-omega.vercel.app</loc>
+    <lastmod>2024-04-06</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  res.status(200).send(xml);
+} 
