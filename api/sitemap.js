439496544d7bb10fab5468e49bd246226f259d91
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2025-04-07 13:37:40 -0300

    refactor: atualiza nome do projeto para calcularbrasil e URLs

--- api/sitemap.js
+++ api/sitemap.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://calculate-dusky-omega.vercel.app</loc>
+    <loc>https://calcularbrasil.com.br</loc>
     <lastmod>2024-04-06</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
